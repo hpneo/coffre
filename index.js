b8f8f67080aafec1c3550b8c1c11a7649e0f802d
HPNeo <hpneo@hotmail.com>
2014-05-14 01:28:46 -0500

    Add associations

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-require('inflector');
-var path = require('path');
-var Connection = require(__dirname + '/lib/connection');
+require('colors');
+
+var Inflector = require('inflecta'),
+    path = require('path'),
+    Connection = require(__dirname + '/lib/connection');
 
 var mappedModels = {},
+    SQL = {
+      tableExists: 'SELECT name FROM sqlite_temp_master WHERE type = ? AND name = ?;'
+    },
     Coffre = {};
-
-var SQL = {
-  tableExists: 'SELECT name FROM sqlite_temp_master WHERE type = ? AND name = ?;'
-};
 
 function setTableName(tableName) {
   this.tableName = tableName;
@@ -17,7 +18,45 @@ function getTableName() {
   return this.tableName;
 }
 
-function defineProperties(modelPrototype) {};
+function hasMany(many, manyOptions) {
+  manyOptions = manyOptions || {};
+
+  this.associations.hasMany[many] = {
+    model: mappedModels[Inflector.tableize(many)] || manyOptions['model'],
+    as: many || manyOptions['as']
+  };
+
+  Object.defineProperty(this.prototype, (many || manyOptions['as']), {
+    configurable: false,
+    get: function() {
+      if (this.associations.hasMany[many] === undefined) {
+        this.associations.hasMany[many] = new Coffre.Relation();
+      }
+
+      return this.associations.hasMany[many];
+    }
+  });
+}
+
+function belongsTo(belongsTo, belongsToOptions) {
+  belongsToOptions = belongsToOptions || {};
+
+  this.associations.belongsTo[belongsTo] = {
+    model: mappedModels[Inflector.tableize(belongsTo)] || belongsToOptions['model'],
+    as: belongsTo || belongsToOptions['as']
+  };
+
+  Object.defineProperty(this.prototype, (belongsTo || belongsToOptions['as']), {
+    configurable: false,
+    get: function() {
+      if (this.associations.belongsTo[belongsTo] === undefined) {
+        this.associations.belongsTo[belongsTo] = new Coffre.Relation();
+      }
+
+      return this.associations.belongsTo[belongsTo];
+    }
+  });
+}
 
 var ModelPrototype = {
   get: function(attribute) {
@@ -26,6 +65,69 @@ var ModelPrototype = {
   set: function(attribute, value) {
     this.attributes[attribute] = value;
   }
+};
+
+Coffre.Relation = function() {};
+
+Coffre.build = function() {
+  var modelName = arguments[0],
+      modelOptions = arguments[1];
+
+  if (modelName === undefined || modelName === "") {
+    throw new Error('Must set a model name');
+  }
+
+  if (modelOptions !== undefined && !(modelOptions instanceof Function)) {
+    throw new Error('Must set the model options as a function');
+  }
+
+  var newModel = function Model(baseAttributes) {
+    this.attributes = {};
+    this.associations = {
+      hasMany: {},
+      belongsTo: {}
+    };
+
+    for (var i in baseAttributes) {
+      this.attributes[i] = baseAttributes[i];
+    }
+  };
+
+  newModel.prototype = ModelPrototype;
+
+  newModel.associations = {
+    hasMany: {},
+    belongsTo: {}
+  };
+
+  newModel.getTableName = getTableName;
+  newModel.setTableName = setTableName;
+  newModel.hasMany = hasMany;
+  newModel.belongsTo = belongsTo;
+
+  newModel.connection = Coffre.getConnection();
+
+  newModel.setTableName(Inflector.tableize(modelName));
+
+  if (modelOptions !== undefined) {
+    modelOptions.call(newModel);
+  }
+
+  if (newModel.connection.database.open === true) {
+    Coffre.tableExists(newModel, function() {
+      // console.log(modelName + '.isMapped', newModel.isMapped);
+    });
+  }
+
+  newModel.connection.database.on('open', function() {
+    Coffre.tableExists(newModel, function() {
+      // console.log(modelName + '.isMapped', newModel.isMapped);
+    });
+  });
+
+  mappedModels[newModel.getTableName()] = newModel;
+
+  return newModel;
 };
 
 Coffre.getConnection = function(env) {
@@ -47,59 +149,10 @@ Coffre.tableExists = function(model, callback) {
 
     callback(model);
   });
-}
-
-Coffre.build = function() {
-  var modelName = arguments[0],
-      modelOptions = arguments[1];
-
-  if (modelName === undefined || modelName === "") {
-    throw new Error('Must set a model name');
-  }
-
-  if (modelOptions !== undefined && !(modelOptions instanceof Function)) {
-    throw new Error('Must set the model options as a function');
-  }
-
-  var newModel = function Model(baseAttributes) {
-    this.attributes = {};
-
-    for (var i in baseAttributes) {
-      this.attributes[i] = baseAttributes[i];
-    }
-  };
-
-  newModel.prototype = ModelPrototype;
-
-  newModel.getTableName = getTableName;
-  newModel.setTableName = setTableName;
-  newModel.connection = Coffre.getConnection();
-
-  newModel.setTableName(modelName.plural());
-
-  if (modelOptions !== undefined) {
-    modelOptions.call(newModel);
-  }
-
-  if (newModel.connection.database.open === true) {
-    Coffre.tableExists(newModel, function() {
-      console.log(modelName + '.isMapped', newModel.isMapped);
-    });
-  }
-
-  newModel.connection.database.on('open', function() {
-    Coffre.tableExists(newModel, function() {
-      console.log(modelName + '.isMapped', newModel.isMapped);
-    });
-  });
-
-  mappedModels[newModel.getTableName()] = newModel;
-
-  return newModel;
 };
 
 Coffre.getModels = function() {
   return mappedModels;
-}
+};
 
 module.exports = Coffre;
--- test/index.js
+++ test/index.js
@@ -4,21 +4,28 @@ var expect = require('chai').expect;
 
 describe('Coffre', function() {
   it('should build models', function() {
-    var User = Coffre.build('User');
+    var User = Coffre.build('User'),
+        Blog = Coffre.build('Blog');
 
     expect(User.getTableName()).to.equal('users');
+    expect(Blog.getTableName()).to.equal('blogs');
   });
 
   it('should build models with extra options', function() {
     var User = Coffre.build('User', function() {
-      this.setTableName('users_users');
-    });
+          this.setTableName('users_users');
+        }),
+        Blog = Coffre.build('Blog', function() {
+          this.setTableName('users_blogs');
+        });
 
     expect(User.getTableName()).to.equal('users_users');
+    expect(Blog.getTableName()).to.equal('users_blogs');
   });
 
   it('should allow build instances of existing models', function() {
-    var User = Coffre.build('User');
+    var User = Coffre.build('User'),
+        Blog = Coffre.build('Blog');
 
     var hpneo = new User({
       firstName: 'Gustavo',
@@ -26,6 +33,35 @@ describe('Coffre', function() {
       email: 'hpneo@hotmail.com'
     });
 
+    var rsi = new Blog({
+      title: 'Reflexiones sin importancia'
+    });
+
     expect(hpneo.get('firstName')).to.equal('Gustavo');
+    expect(rsi.get('title')).to.equal('Reflexiones sin importancia');
+  });
+
+  it('should build models with associations', function() {
+    var User = Coffre.build('User', function() {
+          this.setTableName('users_users');
+          this.hasMany('blogs');
+        }),
+        Blog = Coffre.build('Blog', function() {
+          this.setTableName('users_blogs');
+          this.belongsTo('user');
+        });
+
+    var hpneo = new User({
+      firstName: 'Gustavo',
+      lastName: 'Leon',
+      email: 'hpneo@hotmail.com'
+    });
+
+    var rsi = new Blog({
+      title: 'Reflexiones sin importancia'
+    });
+
+    expect(hpneo.blogs).to.be.an.instanceOf(Coffre.Relation);
+    expect(rsi.user).to.be.an.instanceOf(Coffre.Relation);
   });
 });
