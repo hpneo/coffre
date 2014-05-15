0eb343bc0dfdd1f7cc3c9e6b3a951c16334d2c9d
HPNeo <hpneo@hotmail.com>
2014-05-14 21:42:54 -0500

    Add associations with custom names

---
+++ test/associations.js
@@ -0,0 +1,56 @@
+var Coffre = require(__dirname + '/../index');
+Coffre.ENV = 'test';
+var expect = require('chai').expect;
+
+describe('Coffre', function() {
+  beforeEach(function() {
+    Coffre.clearModels();
+  });
+
+  it('should build models with associations', function() {
+    var User = Coffre.build('User', function() {
+          this.hasMany('blogs');
+        }),
+        Blog = Coffre.build('Blog', function() {
+          this.belongsTo('user');
+        });
+
+    var hpneo = new User({
+      firstName: 'Gustavo',
+      lastName: 'Leon',
+      email: 'hpneo@hotmail.com'
+    });
+
+    var rsi = new Blog({
+      title: 'Reflexiones sin importancia'
+    });
+
+    expect(hpneo.blogs).to.be.an.instanceOf(Coffre.Relation);
+    expect(rsi.user).to.be.an.instanceOf(Coffre.Relation);
+  });
+
+  it('should build models with associations and custom names', function() {
+    var User = Coffre.build('User', function() {
+          this.hasMany('blogs', { as : 'publications' });
+        }),
+        Blog = Coffre.build('Blog', function() {
+          this.belongsTo('user', { as : 'author' });
+        });
+
+    var hpneo = new User({
+      firstName: 'Gustavo',
+      lastName: 'Leon',
+      email: 'hpneo@hotmail.com'
+    });
+
+    var rsi = new Blog({
+      title: 'Reflexiones sin importancia'
+    });
+
+    expect(hpneo.blogs).to.be.undefined;
+    expect(rsi.user).to.be.undefined;
+
+    expect(hpneo.publications).to.be.an.instanceOf(Coffre.Relation);
+    expect(rsi.author).to.be.an.instanceOf(Coffre.Relation);
+  });
+});
--- test/index.js
+++ test/index.js
@@ -3,6 +3,10 @@ Coffre.ENV = 'test';
 var expect = require('chai').expect;
 
 describe('Coffre', function() {
+  beforeEach(function() {
+    Coffre.clearModels();
+  });
+  
   it('should build models', function() {
     var User = Coffre.build('User'),
         Blog = Coffre.build('Blog');
@@ -39,29 +43,5 @@ describe('Coffre', function() {
 
     expect(hpneo.get('firstName')).to.equal('Gustavo');
     expect(rsi.get('title')).to.equal('Reflexiones sin importancia');
-  });
-
-  it('should build models with associations', function() {
-    var User = Coffre.build('User', function() {
-          this.setTableName('users_users');
-          this.hasMany('blogs');
-        }),
-        Blog = Coffre.build('Blog', function() {
-          this.setTableName('users_blogs');
-          this.belongsTo('user');
-        });
-
-    var hpneo = new User({
-      firstName: 'Gustavo',
-      lastName: 'Leon',
-      email: 'hpneo@hotmail.com'
-    });
-
-    var rsi = new Blog({
-      title: 'Reflexiones sin importancia'
-    });
-
-    expect(hpneo.blogs).to.be.an.instanceOf(Coffre.Relation);
-    expect(rsi.user).to.be.an.instanceOf(Coffre.Relation);
   });
 });
